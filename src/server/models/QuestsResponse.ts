ccb7a9779c1f60a9e61f3df80d3c897efbc2fad1
Microsxema <RobertGtr@protonmail.com>
2021-05-18 20:17:17 +0700

    Добавлена модель для ответа на квесты

---
+++ src/server/models/QuestsResponse.ts
@@ -0,0 +1,15 @@
+import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript';
+import { User } from './User';
+import { Quest } from './Quest';
+import { getUUID } from '../utils';
+
+@Table
+export class QuestsResponse extends Model {
+  @Column({ primaryKey: true, type: DataType.STRING, defaultValue: () => getUUID() }) id: string;
+
+  @ForeignKey(() => User) @Column(DataType.STRING) userId: string;
+  @ForeignKey(() => Quest) @Column(DataType.STRING) questId: string;
+
+  @BelongsTo(() => User) user: User;
+  @BelongsTo(() => Quest) quest: Quest;
+}
--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -3,11 +3,12 @@ import { User } from "./User";
 import { Session } from "./Session";
 import { Point } from "./Point";
 import { Quest } from "./Quest";
+import { QuestsResponse } from "./QuestsResponse";
 
 export async function initDatabase(dbLink: string, logging = false, sync = false) {
   const sequelize = new Sequelize(dbLink, {
     dialect: "postgres",
-    models: [User, Session, Point, Quest]
+    models: [User, Session, Point, Quest, QuestsResponse]
   });
   if (sync)
     await sequelize.sync();
