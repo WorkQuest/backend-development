fcb2e282e5368912592964d2fb006c17cee355ed
Microsxema <RobertGtr@protonmail.com>
2021-06-24 15:33:04 +0700

    Добавлена схема для RatingStatistic

--- src/server/schemes/user.ts
+++ src/server/schemes/user.ts
@@ -3,6 +3,7 @@ import { idSchema, isoDateSchema } from './index';
 import { mediaUrlOnlySchema } from './media';
 import { UserRole } from "../models/User";
 import { reviewSchema } from './review';
+import { ratingStatisticSchema } from './ratingStatistic';
 
 const reviewsSchema = Joi.array().items(reviewSchema).label('Reviews');
 
@@ -11,7 +12,6 @@ export const passwordSchema = Joi.string().min(8).max(1000).example("p@ssw0rd").
 export const firstNameSchema = Joi.string().min(1).max(1000).example("ivan").label("UserFirstName");
 export const lastNameSchema = Joi.string().min(1).max(1000).example("ivanov").label("UserLastName");
 export const userRoleSchema = Joi.string().valid(...Object.values(UserRole)).example(UserRole.Worker).label("UserRole");
-export const averageRatingSchema = Joi.number().allow(null).label('AverageRating');
 
 export const socialMediaNicknamesSchema = Joi.object({
   instagram: Joi.string().allow(null).label('Instagram'),
@@ -52,7 +52,7 @@ export const userSchema = Joi.object({
   role: userRoleSchema,
   avatar: mediaUrlOnlySchema.allow(null),
   reviews: reviewsSchema,
-  averageRating: averageRatingSchema,
+  ratingStatistic: ratingStatisticSchema,
   createdAt: isoDateSchema,
   updatedAt: isoDateSchema,
 }).label("UserSchema");
