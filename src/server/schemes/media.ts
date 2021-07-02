f9929ba94300f74d50a528bcb44e144482e8b332
Max Denushev <mdenushev@ya.ru>
2021-07-02 16:36:33 +0700

    [update] Fix schemas

--- src/server/schemes/media.ts
+++ src/server/schemes/media.ts
@@ -1,15 +1,17 @@
 import * as Joi from "joi";
-import { ContentType } from '../models/Media';
-import { idSchema, urlSchema } from './index';
+import { ContentType } from "../models/Media";
+import { idSchema, urlSchema } from "./index";
 
 const mediaIdSchema = idSchema.label("MediaId");
-export const contentTypeSchema = Joi.string().valid(...Object.values(ContentType)).example(ContentType.png).label('ContentType');
-export const mediaHashSchema = Joi.number().min(60).max(60).label('MediaHash');
+export const contentTypeSchema = Joi.string().valid(...Object.values(ContentType)).example(ContentType.png).label("ContentType");
+export const mediaHashSchema = Joi.number().min(60).max(60).label("MediaHash");
+
+export const mediaIdsSchema = Joi.array().items(mediaIdSchema).label("MediaIdsArray");
 
 export const mediaUrlOnlySchema = Joi.object({
   id: mediaIdSchema,
-  url: urlSchema,
-}).label('MediaUrlOnlyScheme');
+  url: urlSchema
+}).label("MediaUrlOnlyScheme");
 
 export const mediaSchema = Joi.object({
   id: mediaIdSchema,
