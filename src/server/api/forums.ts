6fefe0e631ff44a73902211abcefd36e33019ea0
Alexander Bokovoy <robzombieg@gmail.com>
2021-07-23 16:05:26 +0700

    [update] refresh API - DELETE

--- src/server/api/forums.ts
+++ src/server/api/forums.ts
@@ -193,7 +193,8 @@ export async function deleteFile(r) {
     if (!file) {
       return error(404000, "File not found", null);
     }
-  } catch (err) {
+  } catch (err)
+  {
     throw error(500000, "Internal Server Error", null);
   }
   return output();
