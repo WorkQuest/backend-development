30c726b9d384626598f8afb9c124557fe685a59d
Microsxema <RobertGtr@protonmail.com>
2021-08-30 13:25:12 +0700

    [API]: transfer api listMapPoints in getQuests

--- src/server/api/map.ts
+++ src/server/api/map.ts
@@ -1,8 +1,6 @@
 import { output } from "../utils";
 import { searchFields } from './quest';
-import { Quest, QuestsResponse, QuestsResponseType, StarredQuests, User } from "@workquest/database-models/lib/models";
-import { literal, Op } from "sequelize";
-import * as sequelize from "sequelize"
+
 function makeWhere(query) {
   const like = query.q ? ` LIKE '%${query.q}%' ` : '';
 
@@ -50,7 +48,7 @@ export async function mapPoints(r) {
     GROUP BY cid
     Order BY cid) LC;`;
 
-  const [results, metadata] = await r.server.app.db.query(query, {
+  const [results, ] = await r.server.app.db.query(query, {
     replacements: {
       northLongitude: r.query.north.longitude,
       northLatitude: r.query.north.latitude,
@@ -63,84 +61,26 @@ export async function mapPoints(r) {
 }
 
 export async function listMapPoints(r) {
-  // let where = makeWhere(r);
-  // let order = r.query.sort ? makeOrderBy(r.query.sort) : '';
-  // let query = `
-  // SELECT
-  //   id, "userId", status,
-  //   priority, category, location,
-  //   title, description, price,
-  //   "adType", "createdAt", "updatedAt"
-  // FROM
-  // (SELECT * FROM public."Users" WHERE "Quests"."userId" = "Users".id)
-  // public."Quests"
-  // ${where}
-  // ${order}`;
-  //
-  //
-  // const [results, ] = await r.server.app.db.query(query, {
-  //   replacements: {
-  //     northLongitude: r.query.north.longitude,
-  //     northLatitude: r.query.north.latitude,
-  //     southLongitude: r.query.south.longitude,
-  //     southLatitude: r.query.south.latitude,
-  //   }
-  // });
-  const order = [];
-  const include = [];
-  const where = {
-    ...(r.query.performing && { assignedWorkerId: r.auth.credentials.id } ),
-    ...(r.query.priority && { priority: r.query.priority }),
-    ...(r.query.status && { status: r.query.status }),
-    ...(r.query.adType && {adType: r.query.adType}),
-    ...(r.params.userId && { userId: r.params.userId }),
-    [Op.and]: sequelize.literal(`st_within("locationPostGIS", st_makeenvelope(${r.query.north.longitude}, ${r.query.north.latitude}, ${r.query.south.longitude}, ${r.query.south.latitude}, 4326))`)
-  };
+  let where = makeWhere(r);
+  let order = r.query.sort ? makeOrderBy(r.query.sort) : '';
+  let query = `
+  SELECT 
+    id, "userId", status,
+    priority, category, location,
+    title, description, price,
+    "adType", "createdAt", "updatedAt"
+  FROM public."Quests"
+  ${where}
+  ${order}`;
 
-  if (r.query.q) {
-    where[Op.or] = searchFields.map(field => ({
-      [field]: {
-        [Op.iLike]: `%${r.query.q}%`
-      }
-    }))
-  }
-  if (r.query.invited) {
-    include.push({
-      model: QuestsResponse,
-      attributes: [],
-      where: {
-        [Op.and]: [
-          { workerId: r.auth.credentials.id },
-          { type: QuestsResponseType.Invite },
-        ]
-      }
-    });
-  }
-  if (r.query.starred) {
-    include.push({
-      model: StarredQuests,
-      as: 'starredQuests',
-      where: { userId: r.auth.credentials.id },
-      attributes: [],
-    });
-  }
-
-  include.push({
-    model: StarredQuests,
-    as: "star",
-    where: { userId: r.auth.credentials.id },
-    required: false
+  const [results, ] = await r.server.app.db.query(query, {
+    replacements: {
+      northLongitude: r.query.north.longitude,
+      northLatitude: r.query.north.latitude,
+      southLongitude: r.query.south.longitude,
+      southLatitude: r.query.south.latitude,
+    }
   });
 
-  for (const [key, value] of Object.entries(r.query.sort)) {
-    order.push([key, value]);
-  }
-
-  const { count, rows } = await Quest.findAndCountAll({
-    limit: r.query.limit,
-    offset: r.query.offset,
-    include, order, where
-  });
-
-  return output({count, quests: rows});
+  return output({ quests: results });
 }
