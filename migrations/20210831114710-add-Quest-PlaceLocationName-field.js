55e6c628a87dfb5ac584f3428b57ce96dd0b17e8
Irina Prilepova <prilepovaid@mail.ru>
2021-08-31 19:02:28 +0700

    Add migration

---
+++ migrations/20210831114710-add-Quest-PlaceLocationName-field.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Quests', 'locationPlaceName', {
+      type: Sequelize.DataTypes.STRING,
+    })
+  },
+  
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Quests', 'locationPlaceName')
+  }
+};
