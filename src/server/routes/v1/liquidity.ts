87777cdc354e32af440c954fb8ee97b3fb08cc2e
Microsxema <RobertGtr@protonmail.com>
2021-09-15 15:56:12 +0700

    [API]: fix liquidity

--- src/server/routes/v1/liquidity.ts
+++ src/server/routes/v1/liquidity.ts
@@ -1,33 +1,28 @@
 import { getSwapsWQT, getTokenDayData } from "../../api/liquidity";
-import { outputOkSchema, tokenDayWQTSchema,swapWQTSchema } from "@workquest/database-models/lib/schemes";
+import { outputOkSchema, tokenDayWQTSchema, swapWQTSchema } from "@workquest/database-models/lib/schemes";
 
-
-export default [
-  {
-    method: "GET",
-    path: "/v1/liquidity/swaps",
-    handler: getSwapsWQT,
-    options: {
-      auth: 'jwt-access',
-      id: "v1.auth.getSwaps",
-      tags: ["api", "auth", "swaps"],
-      description: "Get the last 100 swaps on a pair by fetching Swap events",
-      response: {
-        schema: outputOkSchema(swapWQTSchema).label("GetSwapsWQTResponse")
-      },
-    }
-  }, {
-    method: "GET",
-    path: "/v1/liquidity/tokenDay",
-    handler: getTokenDayData,
-    options: {
-      auth: 'jwt-access',
-      id: "v1.auth.getTokenData",
-      tags: ["api", "auth", "token"],
-      description: "Get daily information for DAI",
-      response: {
-        schema: outputOkSchema(tokenDayWQTSchema).label("GetSwapsWQTResponse")
-      },
-    }
-  },
-]
+export default [{
+  method: "GET",
+  path: "/v1/liquidity/swaps",
+  handler: getSwapsWQT,
+  options: {
+    id: "v1.liquidity.getSwapsWQT",
+    tags: ["api", "swaps"],
+    description: "Get the last 100 swaps on a pair by fetching Swap events",
+    response: {
+      schema: outputOkSchema(swapWQTSchema).label("GetSwapsWQTResponse")
+    },
+  }
+}, {
+  method: "GET",
+  path: "/v1/liquidity/tokenDay",
+  handler: getTokenDayData,
+  options: {
+    id: "v1.liquidity.getTokenDayData",
+    tags: ["api", "token"],
+    description: "Get daily information for DAI",
+    response: {
+      schema: outputOkSchema(tokenDayWQTSchema).label("GetTokenDayDataResponse")
+    },
+  }
+}]
