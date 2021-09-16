0bfa09920d7cdaabd2fce50b03cfe9e260946b69
Alexander Bokovoy <robzombieg@gmail.com>
2021-09-16 11:41:46 +0700

    [update] all routs (sort by date)

--- src/server/api/liquidity.ts
+++ src/server/api/liquidity.ts
@@ -25,27 +25,23 @@ const pair = new Pair(
   new TokenAmount(WETH, config.token.WETH.amountMax)
 );
 
+const { url, params, query } = {
+  url: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+  params: `orderBy: timestamp, orderDirection: desc, where: { pair: "${pair.liquidityToken.address.toLowerCase()}" }`,
+  query: `transaction { id timestamp } pair { txCount }`
+}
+
+
 export async function getSwaps(r) {
   try {
     const result = await axios({
-      url: `https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2`,
-      method: "POST",
+      url: url,
+      method: 'POST',
       data: {
         query: `{ 
-          swaps(first:${r.query.limit}, skip:${r.query.offset}, orderBy: timestamp, orderDirection: desc, where:
-          { pair: "${pair.liquidityToken.address.toLowerCase()}" }) {
-            pair {
-            txCount
-            token0 {symbol}
-            token1 {symbol}
-            }
-            amount0In
-            amount0Out
-            amount1In
-            amount1Out
-            amountUSD
-            to
-            timestamp
+          swaps(first:${r.query.limit}, skip:${r.query.offset}, ${params} ) {
+            ${query}
+            amount0In amount0Out amount1In amount1Out amountUSD to
           }
         }`
       }
@@ -64,18 +60,13 @@ export async function getSwaps(r) {
 export async function getMints(r) {
   try {
     const result = await axios({
-      url: `https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2`,
+      url: url,
       method: "POST",
       data: {
         query: `{ 
-          mints(first:${r.query.limit}, skip:${r.query.offset}, orderBy: timestamp, orderDirection: desc, where:
-          { pair: "${pair.liquidityToken.address.toLowerCase()}" }) {
-            transaction { id timestamp }
-            to
-            liquidity
-            amount0
-            amount1
-            amountUSD
+          mints(first:${r.query.limit}, skip:${r.query.offset}, ${params}) {
+            ${query}
+            to liquidity amount0 amount1 amountUSD
           }
         }`
       }
@@ -94,18 +85,13 @@ export async function getMints(r) {
 export async function getBurns(r) {
   try {
     const result = await axios({
-      url: `https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2`,
+      url: url,
       method: "POST",
       data: {
         query: `{ 
-          burns(first:${r.query.limit}, skip:${r.query.offset}, orderBy: timestamp, orderDirection: desc, where:
-          { pair: "${pair.liquidityToken.address.toLowerCase()}" }) {
-            transaction { id timestamp }
-            to
-            liquidity
-            amount0
-            amount1
-            amountUSD
+          burns(first:${r.query.limit}, skip:${r.query.offset}, ${params}) {
+            ${query}
+            to liquidity amount0 amount1 amountUSD
           }
         }`
       }
@@ -124,23 +110,15 @@ export async function getBurns(r) {
 export async function getTokenDayData(r) {
   try {
     const result = await axios({
-    url: `https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2`,
+    url: url,
     method: "POST",
     data: {
       query: `{ 
-        tokenDayDatas(first:${r.query.limit}, skip:${r.query.offset},orderBy: date, orderDirection: asc,
+        tokenDayDatas(first:${r.query.limit}, skip:${r.query.offset},orderBy: date, orderDirection: desc,
         where: {
           token: "${WQT.address.toLowerCase()}"
-          }) {
-            id
-            date
-            priceUSD
-            totalLiquidityToken
-            totalLiquidityUSD
-            totalLiquidityETH
-            dailyVolumeETH
-            dailyVolumeToken
-            dailyVolumeUSD
+          }) { id date priceUSD totalLiquidityToken totalLiquidityUSD totalLiquidityETH
+            dailyVolumeETH dailyVolumeToken dailyVolumeUSD
         }
       }`
     }
