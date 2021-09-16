b01bf11cda4c920cf39a4f2c5c41a108fee6af51
Irina Prilepova <prilepovaid@mail.ru>
2021-09-16 17:38:02 +0700

    [migrations]: add migration

---
+++ migrations/20210916103216-add-Quest-placement-field.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Quests', 'workplace', {
+      type: Sequelize.DataTypes.STRING,
+      defaultValue: "distant"
+    })
+  },
+  
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Quests', 'workplace')
+  }
+};
