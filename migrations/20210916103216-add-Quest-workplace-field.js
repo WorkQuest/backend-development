b930b7a439421ed2ecb288f5289f0ea14acdda2e
Microsxema <RobertGtr@protonmail.com>
2021-09-16 18:39:29 +0700

    add migration

--- migrations/20210916103216-add-Quest-workplace-field.js
+++ migrations/20210916103216-add-Quest-workplace-field.js
@@ -4,10 +4,10 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Quests', 'workplace', {
       type: Sequelize.DataTypes.STRING,
-      defaultValue: "distant"
+      defaultValue: 'distant',
     })
   },
-  
+
   down: async (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Quests', 'workplace')
   }
