0bba75276c8ba6988ebd38d447b933a8cf34ecbd
Microsxema <RobertGtr@protonmail.com>
2021-10-08 16:21:27 +0700

    fix

--- src/server/routes/v1/profile.ts
+++ src/server/routes/v1/profile.ts
@@ -24,7 +24,7 @@ import {
   userPasswordSchema,
   userRoleSchema,
   userSchema,
-  userShortSchema
+  userShortSchema,
 } from "@workquest/database-models/lib/schemes";
 import { UserRole } from "@workquest/database-models/lib/models";
 
@@ -41,21 +41,64 @@ export default [{
     }
   }
 }, {
-  method: "POST",
-  path: "/v1/profile/set-role",
-  handler: setRole,
+  method: "GET",
+  path: "/v1/profile/{userId}",
+  handler: getUser,
   options: {
-    id: "v1.profile.setRole",
+    id: "v1.profile.getUser",
     tags: ["api", "profile"],
-    description: "Set role user (Only for need set role)",
+    description: "Get profile user",
     validate: {
-      payload: Joi.object({
-        role: userRoleSchema.required()
-      }).label("SetUserRolePayload")
+      params: Joi.object({
+        userId: idSchema.required(),
+      }).label("GetUserParams"),
     },
     response: {
-      schema: emptyOkSchema
+      schema: outputOkSchema(userSchema).label("GetUserResponse")
     }
+  }
+}, {
+  method: "POST",
+  path: "/v1/profile/employers/get",
+  handler: getUsers(UserRole.Employer),
+  options: {
+    id: "v1.profile.getEmployers",
+    tags: ["api", "profile"],
+    description: "Get employers with filters",
+    validate: {
+      query: employerQuerySchema,
+      payload: Joi.object({
+        location: Joi.object({
+          north: locationSchema,
+          south: locationSchema,
+        }).label('GetEmployerLocation'),
+      }).label('GetEmployersPayload'),
+    },
+    response: {
+      schema: outputOkSchema(userShortSchema).label("GetQuestsResponse")
+    },
+  }
+}, {
+  method: "POST",
+  path: "/v1/profile/workers/get",
+  handler: getUsers(UserRole.Worker),
+  options: {
+    id: "v1.profile.getWorkers",
+    tags: ["api", "profile"],
+    description: "Get workers with filters",
+    validate: {
+      query: employerQuerySchema,
+      payload: Joi.object({
+        skillFilters: skillFilterSchema,
+        location: Joi.object({
+          north: locationSchema,
+          south: locationSchema,
+        }).label('GetWorkersLocation'),
+      }).label('GetWorkersPayload'),
+    },
+    response: {
+      schema: outputOkSchema(userShortSchema).label("GetQuestsResponse")
+    },
   }
 }, {
   method: "PUT",
@@ -84,6 +127,50 @@ export default [{
   }
 }, {
   method: "PUT",
+  path: "/v1/profile/employer/edit",
+  handler: editProfile(UserRole.Employer),
+  options: {
+    id: "v1.profile.editEmployer",
+    tags: ["api", "profile"],
+    description: "Edit employer profile information",
+    validate: {
+      payload: Joi.object({
+        avatarId: idSchema.allow(null).required(),
+        firstName: userFirstNameSchema.required(),
+        lastName: userLastNameSchema.required(),
+        location: locationSchema.allow(null).required(),
+        skillFilters: skillFilterSchema.allow(null).required(),
+        additionalInfo: userAdditionalInfoEmployerSchema.required(),
+      }).label("EditEmployerProfilePayload")
+    },
+    response: {
+      schema: outputOkSchema(userSchema).label("UserResponse")
+    }
+  }
+}, {
+  method: "PUT",
+  path: "/v1/profile/worker/edit",
+  handler: editProfile(UserRole.Worker),
+  options: {
+    id: "v1.profile.editWorker",
+    tags: ["api", "profile"],
+    description: "Edit worker profile information",
+    validate: {
+      payload: Joi.object({
+        avatarId: idSchema.allow(null).required(),
+        firstName: userFirstNameSchema.required(),
+        lastName: userLastNameSchema.required(),
+        location: locationSchema.allow(null).required(),
+        skillFilters: skillFilterSchema.allow(null).required(),
+        additionalInfo: userAdditionalInfoWorkerSchema.required(),
+      }).label("EditWorkerProfilePayload")
+    },
+    response: {
+      schema: outputOkSchema(userSchema).label("UserResponse")
+    }
+  }
+}, {
+  method: "PUT",
   path: "/v1/profile/change-password",
   handler: changePassword,
   options: {
@@ -95,6 +182,23 @@ export default [{
         oldPassword: userPasswordSchema.required(),
         newPassword: userPasswordSchema.required()
       }).label("ChangePasswordPayload")
+    },
+    response: {
+      schema: emptyOkSchema
+    }
+  }
+}, {
+  method: "POST",
+  path: "/v1/profile/set-role",
+  handler: setRole,
+  options: {
+    id: "v1.profile.setRole",
+    tags: ["api", "profile"],
+    description: "Set role user (Only for need set role)",
+    validate: {
+      payload: Joi.object({
+        role: userRoleSchema.required()
+      }).label("SetUserRolePayload")
     },
     response: {
       schema: emptyOkSchema
@@ -134,109 +238,5 @@ export default [{
       schema: emptyOkSchema
     }
   }
-}, {
-  method: "GET",
-  path: "/v1/profile/{userId}",
-  handler: getUser,
-  options: {
-    id: "v1.profile.getUser",
-    tags: ["api", "profile"],
-    description: "Get profile user",
-    validate: {
-      params: Joi.object({
-        userId: idSchema.required(),
-      }).label("GetUserParams"),
-    },
-    response: {
-      schema: outputOkSchema(userSchema).label("GetUserResponse")
-    }
-  }
-}, {
-  method: "PUT",
-  path: "/v1/profile/employer/edit",
-  handler: editProfile(UserRole.Employer),
-  options: {
-      id: "v1.employer.profile.edit",
-      tags: ["api", "profile"],
-      description: "Edit employer profile information",
-      validate: {
-        payload: Joi.object({
-          avatarId: idSchema.allow(null).required(),
-          firstName: userFirstNameSchema.required(),
-          lastName: userLastNameSchema.required(),
-          location: locationSchema.allow(null).required(),
-          skillFilters: skillFilterSchema.allow(null).required(),
-          additionalInfo: userAdditionalInfoEmployerSchema.required(),
-        }).label("EditEmployerProfilePayload")
-      },
-      response: {
-        schema: outputOkSchema(userSchema).label("UserResponse")
-      }
-    }
-}, {
-  method: "PUT",
-  path: "/v1/profile/worker/edit",
-  handler: editProfile(UserRole.Worker),
-  options: {
-    id: "v1.worker.profile.edit",
-    tags: ["api", "profile"],
-    description: "Edit worker profile information",
-    validate: {
-      payload: Joi.object({
-        avatarId: idSchema.allow(null).required(),
-        firstName: userFirstNameSchema.required(),
-        lastName: userLastNameSchema.required(),
-        location: locationSchema.allow(null).required(),
-        skillFilters: skillFilterSchema.allow(null).required(),
-        additionalInfo: userAdditionalInfoWorkerSchema.required(),
-      }).label("EditWorkerProfilePayload")
-    },
-    response: {
-      schema: outputOkSchema(userSchema).label("UserResponse")
-    }
-  }
-}, {
-  method: "POST",
-  path: "/v1/employers/get",
-  handler: getUsers(UserRole.Employer),
-  options: {
-    id: "v1.getEmployers",
-    tags: ["api", "profile"],
-    description: "Get employers with filters",
-    validate: {
-      query: employerQuerySchema,
-      payload: Joi.object({
-        location: Joi.object({
-          north: locationSchema,
-          south: locationSchema,
-        }).label('GetEmployerLocation'),
-      }).label('GetEmployersPayload'),
-    },
-    response: {
-      schema: outputOkSchema(userShortSchema).label("GetQuestsResponse")
-    },
-  }
-}, {
-  method: "POST",
-  path: "/v1/workers/get",
-  handler: getUsers(UserRole.Worker),
-  options: {
-    id: "v1.getWorkers",
-    tags: ["api", "profile"],
-    description: "Get workers with filters",
-    validate: {
-      query: employerQuerySchema,
-      payload: Joi.object({
-        skillFilters: skillFilterSchema,
-        location: Joi.object({
-          north: locationSchema,
-          south: locationSchema,
-        }).label('GetWorkersLocation'),
-      }).label('GetWorkersPayload'),
-    },
-    response: {
-      schema: outputOkSchema(userShortSchema).label("GetQuestsResponse")
-    },
-  }
-},];
+}];
 
