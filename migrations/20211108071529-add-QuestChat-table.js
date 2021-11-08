da8266f4138ae89d254e4b22aa6d0262f225d5d2
Irina Prilepova <prilepovaid@mail.ru>
2021-11-08 14:29:02 +0700

    Add new migration for creating QuestChat table

---
+++ migrations/20211108071529-add-QuestChat-table.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('QuestChats', {
+      id: {
+        type: Sequelize.DataTypes.STRING,
+        primaryKey: true
+      },
+      questId: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Quests',
+            schema: 'public'
+          },
+          key: 'id'
+        }
+      },
+      responseId: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'QuestsResponses',
+            schema: 'public'
+          },
+          key: 'id'
+        }
+      },
+      chatId: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Chats',
+            schema: 'public'
+          },
+          key: 'id'
+        }
+      },
+      isActive: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      createdAt: {
+        type: Sequelize.DataTypes.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DataTypes.DATE
+      }
+    });
+  },
+  
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('QuestChats');
+  }
+};
