1f773cb2b95672e994b8f677c330f0165ab93d99
Irina Prilepova <prilepovaid@mail.ru>
2021-11-09 11:34:24 +0700

    Add logic for QuestChat in inviteOnQuest

--- src/server/api/questsResponse.ts
+++ src/server/api/questsResponse.ts
@@ -29,8 +29,6 @@ export async function responseOnQuest(r) {
   await questController.questMustHaveStatus(QuestStatus.Created);
   await workerController.userMustHaveRole(UserRole.Worker);
 
-  const transaction = await r.server.app.db.transaction();
-
   const questsResponse = await QuestsResponse.findOne({
     where: {
       questId: quest.id,
@@ -41,6 +39,8 @@ export async function responseOnQuest(r) {
   if (questsResponse) {
     return error(Errors.AlreadyAnswer, "You already answered quest", { questsResponse });
   }
+
+  const transaction = await r.server.app.db.transaction();
 
   const response = await QuestsResponse.create({
     workerId: worker.id,
@@ -55,7 +55,7 @@ export async function responseOnQuest(r) {
   },  transaction );
 
   const message = await Message.build({
-    senderUserId: r.auth.credentials.id,
+    senderUserId: worker.id,
     chatId: chat.id,
     type: MessageType.info,
   }, transaction);
@@ -121,13 +121,61 @@ export async function inviteOnQuest(r) {
     return error(Errors.AlreadyAnswer, "You already answered quest", { questResponse });
   }
 
-  await QuestsResponse.create({
+  const transaction = await r.server.app.db.transaction();
+
+  const response = await QuestsResponse.create({
     workerId: invitedWorker.id,
     questId: quest.id,
     message: r.payload.message,
     status: QuestsResponseStatus.Open,
     type: QuestsResponseType.Invite,
+  }, {transaction});
+
+  const chat = await Chat.build({
+    type: ChatType.quest,
+  }, transaction);
+
+  const message = await Message.build({
+    senderUserId: employer.id,
+    chatId: chat.id,
+    type: MessageType.info
+  }, transaction);
+
+  const infoMessage = await InfoMessage.build({
+    messageId: message.id,
+    messageAction: MessageAction.questChatCreate,
   });
+
+  await Promise.all([
+    message.save({ transaction }),
+    infoMessage.save({ transaction }),
+  ]);
+
+  await ChatMember.bulkCreate([{
+    unreadCountMessages: 0, /** Because created */
+    chatId: chat.id,
+    userId: r.auth.credentials.id,
+    lastReadMessageId: message.id, /** Because created,  */
+    lastReadMessageNumber: message.number,
+  }, {
+    unreadCountMessages: 1, /** Because created */
+    chatId: chat.id,
+    userId: invitedWorker.id,
+    lastReadMessageId: null, /** Because created */
+    lastReadMessageNumber: null,
+  }], { transaction });
+
+  chat.lastMessageId = message.id;
+  chat.lastMessageDate = message.createdAt;
+  await chat.save({transaction});
+
+  await QuestChat.create({
+    questId: quest.id,
+    responseId: response.id,
+    chatId: chat.id,
+  }, { transaction });
+
+  await transaction.commit();
 
   return output();
 }
