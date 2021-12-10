998e59076523f6df0c1e78c1c137039e361ab86c
Alexander Bokovoy <robzombieg@gmail.com>
2021-12-10 15:35:56 +0700

    [feature] update routes proposal

--- src/server/api/proposal.ts
+++ src/server/api/proposal.ts
@@ -1,41 +1,24 @@
 import {output} from "../utils";
 import {MediaController} from "../controllers/controller.media";
 import {
-  User,
   Proposal,
-  ProposalMedia,
   ProposalStatus
 } from "@workquest/database-models/lib/models";
 
 //TODO: improve userId to address of user's waller
 export async function createProposal(r) {
-  await MediaController.getMedias(r.payload.medias);
+  const medias = await MediaController.getMedias(r.payload.medias);
 
   const transaction = await r.server.app.db.transaction();
 
-  const proposal = await Proposal.build({
+  const proposal = await Proposal.create({
     userId: r.auth.credentials.id,
     title: r.payload.title,
     description: r.payload.description,
     status: ProposalStatus.Pending,
-    txHash: r.payload.hash,
-  }, );
-
-  const medias = r.payload.medias.map(media => {
-    return {
-      mediaId: media.id,
-      proposalId: proposal.id,
-    }
-  });
-
-  const mediaPreCreate = ProposalMedia.bulkBuild(medias);
+  }, { transaction });
 
   await proposal.$set("medias", medias, { transaction });
-
-  await Promise.all([
-    proposal.save({ transaction }),
-    mediaPreCreate.map(member => member.save({ transaction })),
-  ]);
 
   transaction.commit();
 
