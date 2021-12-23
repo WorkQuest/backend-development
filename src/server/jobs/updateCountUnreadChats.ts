223bed1ce096e85681c787f026548f36d9516f4d
Irina Prilepova <prilepovaid@mail.ru>
2021-12-23 15:57:33 +0700

    build(jobs): create job for update count unread chats

---
+++ src/server/jobs/updateCountUnreadChats.ts
@@ -0,0 +1,23 @@
+import { addJob } from "../utils/scheduler";
+import { ChatMember, ChatsStatistic } from "@workquest/database-models/lib/models";
+import { Op } from "sequelize";
+
+export type UserUnreadChatsPayload = {
+  userId: string,
+}
+
+export async function updateCountUnreadMessagesJob(payload: UserUnreadChatsPayload) {
+  return addJob("updateCountUnreadMessages", payload);
+}
+
+export default async function updateCountUnreadMessages(payload: UserUnreadChatsPayload) {
+  const unreadChatsCounter = await ChatMember.unscoped().count({
+    where: {
+      userId: payload.userId,
+      unreadCountMessages: {[Op.ne]: 0},
+    },
+  });
+
+  await ChatsStatistic.update({ unreadMessages: unreadChatsCounter }, { where: { userId: payload.userId } });
+}
+
