dc4c01a3287a3cf9ff70fa159f14be32a430fec5
Irina Prilepova <prilepovaid@mail.ru>
2022-01-17 17:49:57 +0700

    refactor(raiseView)

--- src/server/api/questRaiseView.ts
+++ src/server/api/questRaiseView.ts
@@ -1,17 +1,13 @@
 import { UserController } from "../controllers/user/controller.user";
 import { QuestController } from "../controllers/quest/controller.quest";
 import { output } from "../utils";
-import {
-  User,
-  Quest,
-  UserRole,
-  QuestRaiseView,
-} from "@workquest/database-models/lib/models";
+import { Quest, QuestRaiseView, User, UserRole } from "@workquest/database-models/lib/models";
 
 //TODO: проверка на то, вышел срок подписки или нет
 export async function createRaiseView(r) {
   const employer: User = r.auth.credentials;
   const userController = new UserController(employer);
+  userController.userMustHaveRole(UserRole.Employer);
 
   const questController = new QuestController(await Quest.findByPk(r.params.questId));
 
@@ -20,18 +16,11 @@ export async function createRaiseView(r) {
 
   await questController.checkQuestRaiseViews();
 
-  await QuestRaiseView.create({
+  const questRaiseView = await QuestRaiseView.create({
     questId: r.params.questId,
     userId: r.auth.credentials.id,
     duration: r.payload.duration,
     type: r.payload.type,
   });
-
-  const questRaiseSchema = await QuestRaiseView.create({
-    questId: r.params.questId,
-    userId: r.auth.credentials.id,
-    duration: r.payload.duration,
-    type: r.payload.type,
-  });
-  return output(questRaiseSchema);
+  return output(questRaiseView);
 }
