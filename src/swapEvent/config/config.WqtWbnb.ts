46e24fa5cb9848d3b6a83e321236462167f91573
Microsxema <RobertGtr@protonmail.com>
2022-01-18 19:11:06 +0700

    refactoring

--- src/swapEvent/config/config.WqtWbnb.ts
+++ src/swapEvent/config/config.WqtWbnb.ts
@@ -3,7 +3,7 @@ import {config} from "dotenv";
 config({ path: __dirname +  '/../../../.env.WqtWbnb' });
 
 export default {
-  contractAddress: process.env.WQT_WBNB_CONTRACT_ADDRESS,
   wsProvider: process.env.WQT_WBNB_WEBSOCKET_PROVIDER,
+  contractAddress: process.env.WQT_WBNB_CONTRACT_ADDRESS,
   parseEventsFromHeight: parseInt(process.env.WQT_WBNB_PARSE_EVENTS_FROM_HEIGHT), // 11335760
 }
