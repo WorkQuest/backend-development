f4cc56200a2e14c8c5cbb3db0820e88d3d3fd976
Irina Prilepova <prilepovaid@mail.ru>
2022-01-27 16:38:43 +0700

    commit to checkout

--- src/server/jobs/updateQuestRaiseViewStatus.ts
+++ src/server/jobs/updateQuestRaiseViewStatus.ts
@@ -1,5 +1,6 @@
 import {addJob} from "../utils/scheduler";
 import {Quest, QuestRaiseStatus, QuestRaiseType, QuestRaiseView} from "@workquest/database-models/lib/models";
+import { Helpers } from "graphile-worker";
 
 export type QuestRaiseViewPayload = {
   questId: string,
@@ -14,9 +15,14 @@ export async function updateQuestRaiseViewStatusJob() {
   return addJob("updateQuestRaiseViewStatus");
 }
 //payload: QuestRaiseViewPayload
-export default async function updateQuestRaiseViewStatus() {
-  addJob('update', {}, {})
+export default async function updateQuestRaiseViewStatus(payload,h: Helpers) {
+  const endOfRaiseView = new Date();
+  const a = endOfRaiseView.setTime(1643274960*1000)
+  console.log(new Date(a));
+  endOfRaiseView.setDate(endOfRaiseView.getDate() + 1);
 
+  console.log("HELLO");
+  await h.addJob('test', {}, {runAt: endOfRaiseView})
   // await QuestRaiseView.update({ status: payload.status }, { where: { questId: payload.questId } });
   // await Quest.update({adType: QuestRaiseType}, {where: {id: payload.questId}});
 
