1c014fbf064802533ce54977a797aa7a6bee6742
Microsxema <RobertGtr@protonmail.com>
2022-01-28 18:31:48 +0700

    [update]

--- src/server/api/liquidityPool(wqt-wbnb).ts
+++ src/server/api/liquidityPool(wqt-wbnb).ts
@@ -1,47 +1,8 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import config from '../config/config';
-import BigNumber from 'bignumber.js';
 import { output } from '../utils';
-import { ChainId, Token, TokenAmount, Pair } from '@pancakeswap/sdk';
-import { PancakeSwapApi } from '../controllers/controller.pancakeSwap';
-import { CoingeckoApi } from '../controllers/controller.coingecko';
-import { DailyLiquidity, WqtWbnbSwapEvent } from '@workquest/database-models/lib/models';
-
-const Web3 = require('web3');
-
-const WQT = new Token(
-  ChainId.MAINNET,
-  config.token.WQT.bscNetwork.address,
-  config.token.WQT.bscNetwork.decimals,
-  config.token.WQT.bscNetwork.symbol,
-  config.token.WQT.bscNetwork.name,
-);
-
-const WBNB = new Token(ChainId.MAINNET, config.token.WBNB.address, config.token.WBNB.decimals, config.token.WBNB.symbol, config.token.WBNB.name);
-
-const pair = new Pair(new TokenAmount(WQT, config.token.WQT.bscNetwork.amountMax), new TokenAmount(WBNB, config.token.WBNB.amountMax));
-
-const pancakeSwapApi = new PancakeSwapApi(pair);
-const coingeckoApi = new CoingeckoApi();
-
-const liquidityMiningProvider = new Web3(
-  new Web3.providers.WebsocketProvider(config.contracts.liquidityMining.webSocketProvider, {
-    clientConfig: {
-      keepalive: true,
-      keepaliveInterval: 60000, // ms
-    },
-    reconnect: {
-      auto: true,
-      delay: 1000, // ms
-      onTimeout: false,
-    },
-  }),
-);
-
-const liquidityMiningAbiPath = path.join(__dirname, '../abi/WQLiquidityMining.json');
-const liquidityMiningAbi: [] = JSON.parse(fs.readFileSync(liquidityMiningAbiPath).toString()).abi;
-const liquidityMiningContract = new liquidityMiningProvider.eth.Contract(liquidityMiningAbi, config.contracts.liquidityMining.contract);
+import {
+  DailyLiquidity,
+  WqtWbnbSwapEvent,
+} from '@workquest/database-models/lib/models';
 
 export async function getSwaps(r) {
   const { count, rows } = await WqtWbnbSwapEvent.findAndCountAll({
@@ -53,24 +14,6 @@ export async function getSwaps(r) {
   return output({ count, swaps: rows });
 }
 
-export async function getMints(r) {
-  const mints = await pancakeSwapApi.getMints({
-    limit: r.query.limit,
-    offset: r.query.offset,
-  });
-
-  return output(mints);
-}
-
-export async function getBurns(r) {
-  const burns = await pancakeSwapApi.getBurns({
-    limit: r.query.limit,
-    offset: r.query.offset,
-  });
-
-  return output(burns);
-}
-
 export async function getTokenDayData(r) {
   const { count, rows } = await DailyLiquidity.findAndCountAll({
     limit: r.query.limit,
@@ -79,25 +22,4 @@ export async function getTokenDayData(r) {
   });
 
   return output({ count, infoPer10Days: rows });
-}
-
-export async function getDistribution(r) {
-  const stakingInfoEvent = await liquidityMiningContract.methods.getStakingInfo().call();
-  const totalStaked = new BigNumber(stakingInfoEvent.totalStaked).shiftedBy(-18).toNumber();
-  const rewardTotal = new BigNumber(stakingInfoEvent.rewardTotal).shiftedBy(-18).toNumber();
-
-  const UsdPrice = await coingeckoApi.getUsdPrice();
-
-  const tokenDayData = await pancakeSwapApi.getTokenDayData({ limit: 1, offset: 0 });
-
-  const reserveUSD = tokenDayData[0].reserveUSD;
-  const totalSupply = tokenDayData[0].totalSupply;
-
-  return output({
-    rewardTotal: rewardTotal.toString(),
-    priceUSD: UsdPrice.toString(),
-    totalStaked: totalStaked.toString(),
-    reserveUSD: reserveUSD,
-    totalSupply: totalSupply,
-  });
 }
--- src/server/routes/v1/liquidityPool(wqt-wbnb).ts
+++ src/server/routes/v1/liquidityPool(wqt-wbnb).ts
@@ -1,12 +1,10 @@
 import * as Joi from 'joi';
-import { getBurns, getMints, getSwaps, getTokenDayData, getDistribution } from '../../api/liquidityPool(wqt-wbnb)';
+import * as handlers from '../../api/liquidityPool(wqt-wbnb)';
 import {
   limitSchema,
   offsetSchema,
-  swapWQTSchema,
   outputOkSchema,
   dailyLiquiditySchema,
-  contractAmountSchema,
   wqtWbnbSwapEventsSchema,
 } from '@workquest/database-models/lib/schemes';
 
@@ -14,7 +12,7 @@ export default [
   {
     method: 'GET',
     path: '/v1/pool-liquidity/wqt-wbnb/swaps',
-    handler: getSwaps,
+    handler: handlers.getSwaps,
     options: {
       auth: false,
       id: 'v1.liquidity.wqt-wbnb.getSwaps',
@@ -33,48 +31,8 @@ export default [
   },
   {
     method: 'GET',
-    path: '/v1/pool-liquidity/wqt-wbnb/mints',
-    handler: getMints,
-    options: {
-      auth: false,
-      id: 'v1.liquidity.wqt-wbnb.getMints',
-      tags: ['api', 'pool-liquidity'],
-      description: 'Get mints on a pair by fetching Mints events',
-      validate: {
-        query: Joi.object({
-          offset: offsetSchema,
-          limit: limitSchema,
-        }).label('GetMintsCounts'),
-      },
-      response: {
-        schema: outputOkSchema(swapWQTSchema).label('GetMintsWQTResponse'),
-      },
-    },
-  },
-  {
-    method: 'GET',
-    path: '/v1/pool-liquidity/wqt-wbnb/burns',
-    handler: getBurns,
-    options: {
-      auth: false,
-      id: 'v1.liquidity.wqt-wbnb.getSBurns',
-      tags: ['api', 'pool-liquidity'],
-      description: 'Get burns on a pair by fetching Burns events',
-      validate: {
-        query: Joi.object({
-          offset: offsetSchema,
-          limit: limitSchema,
-        }).label('GetBurnsCounts'),
-      },
-      response: {
-        schema: outputOkSchema(swapWQTSchema).label('GetBurnsWQTResponse'),
-      },
-    },
-  },
-  {
-    method: 'GET',
     path: '/v1/pool-liquidity/wqt-wbnb/tokenDay',
-    handler: getTokenDayData,
+    handler: handlers.getTokenDayData,
     options: {
       auth: false,
       id: 'v1.liquidity.wqt-wbnb.getTokenDayData',
@@ -88,20 +46,6 @@ export default [
       },
       response: {
         schema: outputOkSchema(dailyLiquiditySchema).label('GetTokenDayDataResponse'),
-      },
-    },
-  },
-  {
-    method: 'GET',
-    path: '/v1/pool-liquidity/wqt-wbnb/distribution',
-    handler: getDistribution,
-    options: {
-      auth: false,
-      id: 'v1.liquidity.wqt-wbnb.getDistribution',
-      tags: ['api', 'pool-liquidity'],
-      description: 'Distribution of the WQT to users',
-      response: {
-        schema: outputOkSchema(contractAmountSchema).label('GetTokenDayDataResponse'),
       },
     },
   },
