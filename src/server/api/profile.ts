2246123cb79c6e75543e7fe52145b6ca47a9b55e
Microsxema <robertgtr@protonmail.com>
2022-02-04 18:33:10 +0700

    feature: user change role

--- src/server/api/profile.ts
+++ src/server/api/profile.ts
@@ -13,8 +13,15 @@ import {
   UserRole,
   ChatsStatistic,
   RatingStatistic,
-  QuestsStatistic, UserStatus
+  QuestsStatistic,
+  UserStatus,
+  QuestStatus,
+  Quest,
+  QuestsResponse,
+  QuestsResponseStatus,
+  AdminChangeRole
 } from "@workquest/database-models/lib/models";
+import { updateQuestsStatisticJob } from '../jobs/updateQuestsStatistic';
 
 export const searchFields = [
   "firstName",
@@ -293,4 +300,119 @@ export async function getUserStatistics(r) {
   });
 
   return output({ chatsStatistic, questsStatistic, ratingStatistic });
+}
+
+
+export async function userChangeRole(r): Promise<unknown> {
+  const transaction = await r.server.app.db.transaction();
+  const { id } = r.auth.credentials;
+  const { role, totp } = r.payload
+  let invalidQuestStatus = [];
+
+
+  const user = await User.scope('withPassword').findByPk(id)
+  const userController = new UserController(user);
+
+  if (user.role === role) {
+    throw error(Errors.InvalidRole, "The user is already assigned this role", {
+      currentRole: user.role,
+      newRole: role,
+    });
+  }
+
+  await userController.userMustHaveActiveStatusTOTP(false);
+  await userController.checkTotpConfirmationCode(totp);
+
+  if (user.role === UserRole.Employer) {
+    const {rows} = await Quest.scope('defaultScope').findAndCountAll({
+      where: {userId: user.id}
+    })
+    for (const quest of rows) {
+      if (!(quest.status === QuestStatus.Done || quest.status === QuestStatus.Closed || quest.status === QuestStatus.Blocked)) {
+        invalidQuestStatus.push({questId: quest.id, status: QuestStatus[quest.status]})
+      }
+    }
+    if (invalidQuestStatus.length !== 0) {
+      return error(Errors.InvalidStatus, "Quest status does not match, it should be disabled", {
+        invalidQuests: invalidQuestStatus
+      });
+    }
+  }
+
+  if (user.role === UserRole.Worker) {
+    const {rows} = await QuestsResponse.scope('defaultScope').findAndCountAll({
+      where: {workerId: user.id}
+    })
+    for (const response of rows) {
+      if (!(response.status === QuestsResponseStatus.Rejected || response.status === QuestsResponseStatus.Closed)) {
+        invalidQuestStatus.push({questResponseId: response.id, responseStatus: QuestsResponseStatus[response.status]})
+      }
+    }
+    if (invalidQuestStatus.length !== 0) {
+      return error(Errors.InvalidStatus, "The status of the response to the quest does not match, it should be changed", {
+        invalidQuests: invalidQuestStatus
+      });
+    }
+  }
+
+  const [changeRole, isCreated] = await AdminChangeRole.findOrCreate({
+    where: {userId: user.id},
+    defaults: {
+      adminId: r.auth.credentials.id,
+      userId: user.id,
+      role: user.role,
+      additionalInfo: user.additionalInfo,
+      wagePerHour: user.wagePerHour,
+      workplace: user.workplace,
+      priority: user.priority
+    }, transaction,
+  })
+
+  if (!isCreated) {
+    const timeDiff = Math.abs((new Date).getTime() - (changeRole.updatedAt).getTime());
+    const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    if (diffDays < 31) {
+      return error(Errors.Forbidden, "Difference in days does not meet requirements", {
+        daysAgo: diffDays
+      });
+    }
+    await user.update({
+      additionalInfo: changeRole.additionalInfo,
+      wagePerHour: changeRole.wagePerHour,
+      workplace: changeRole.workplace,
+      priority: changeRole.priority
+    }, {transaction})
+
+    await changeRole.update({
+      adminId: r.auth.credentials.id,
+      role: user.role,
+      additionalInfo: user.additionalInfo,
+      wagePerHour: user.wagePerHour,
+      workplace: user.workplace,
+      priority: user.priority
+    }, {transaction})
+  }
+
+  if (isCreated) {
+    await userController.setRole(r.payload.role, transaction)
+  }
+
+  await transaction.commit();
+
+  await addUpdateReviewStatisticsJob({
+    userId: user.id,
+  });
+  await updateQuestsStatisticJob({
+    userId: user.id,
+    role: user.role,
+  });
+
+  await transaction.commit()
+
+  return output({
+    adminId: changeRole.adminId,
+    userId: changeRole.userId,
+    role: changeRole.role,
+    additionalInfo: changeRole.additionalInfo,
+  });
 }
