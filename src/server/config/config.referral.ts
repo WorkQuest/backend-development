26f178eec71d296059ae865f88400337a4335909
Alexander Bokovoy <robzombieg@gmail.com>
2022-03-04 13:10:54 +0700

    [update] config referral

--- src/server/config/config.referral.ts
+++ src/server/config/config.referral.ts
@@ -4,6 +4,5 @@ config({ path: __dirname + '/../../../.env.referral' });
 
 export default {
   debug: process.env.BRIDGE_DEBUG === 'true',
-  wssProviderLink: process.env.REFERRAL_WEBSOCKET_PROVIDER,
   privateKey: process.env.REFERRAL_CONTRACT_PRIVAT_KEY,
 };
