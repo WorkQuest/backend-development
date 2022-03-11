fce597de47798ca21ae5d57bed8315dfb2646dd1
Mixrosxema <RobertGtr@protonmail.com>
2022-03-11 17:03:28 +0700

    update

--- src/server/api/profile.ts
+++ src/server/api/profile.ts
@@ -57,15 +57,6 @@ export async function getUser(r) {
 export async function getAllUsers(r) {
   const where = { status: UserStatus.Confirmed };
 
-  const userRaiseViewLiteral = literal(
-    '(SELECT "type" FROM "UserRaiseViews" WHERE "userId" = "User"."id" AND "UserRaiseViews"."status" = 0)'
-  );
-  const userRatingStatisticLiteral = literal(
-    '(SELECT "status" FROM "RatingStatistics" WHERE "userId" = "User"."id")'
-  );
-
-  const order = [[userRaiseViewLiteral, 'asc'], [userRatingStatisticLiteral, 'asc']] as any;
-
   if (r.query.q) {
     where[Op.or] = searchFields.map(
       field => ({ [field]: { [Op.iLike]: `%${r.query.q}%` }})
@@ -76,7 +67,6 @@ export async function getAllUsers(r) {
     where,
     distinct: true,
     col: '"User"."id"',
-    order,
     limit: r.query.limit,
     offset: r.query.offset,
   });
