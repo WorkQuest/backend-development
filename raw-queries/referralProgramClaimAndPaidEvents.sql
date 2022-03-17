732fcdcca2bc4b602915d5705fcc5bce6e85a6e7
Alexander Bokovoy <robzombieg@gmail.com>
2022-03-17 16:41:47 +0700

    update GetMeUser, add My referral code

--- raw-queries/referralProgramClaimAndPaidEvents.sql
+++ raw-queries/referralProgramClaimAndPaidEvents.sql
@@ -5,20 +5,37 @@ FROM (SELECT "blockNumber",
              "affiliate",
              "amount",
              "timestamp",
-             'PaidReferral' as "event"
-      FROM "ReferralProgramEventPaidReferrals"
+             'PaidReferral'         as "event",
+             "firstName",
+             "lastName",
+             "avatar"."id"          as "avatar.id",
+             "avatar"."url"         as "avatar.url",
+             "avatar"."contentType" as "avatar.contentType"
+      FROM "Users"
+               LEFT JOIN "Media" as "avatar" ON "avatar"."id" = "Users"."avatarId",
+           "ReferralProgramEventPaidReferrals"
+               INNER JOIN "Wallets" ON "Wallets"."address" = "referral"
       WHERE "affiliate" = :affiliate
+        AND "Users"."id" = "Wallets"."userId"
       UNION
       SELECT "blockNumber",
              "transactionHash",
-             null            as "referral",
+             null                   as "referral",
              "affiliate",
              "amount",
              "timestamp",
-             'RewardClaimed' as "event"
-      FROM "ReferralProgramEventRewardClaimeds"
+             'RewardClaimed'        as "event",
+             "firstName",
+             "lastName",
+             "avatar"."id"          as "avatar.id",
+             "avatar"."url"         as "avatar.url",
+             "avatar"."contentType" as "avatar.contentType"
+      FROM "Users"
+               LEFT JOIN "Media" as "avatar" ON "avatar"."id" = "Users"."avatarId",
+           "ReferralProgramEventPaidReferrals"
+               INNER JOIN "Wallets" ON "Wallets"."address" = "referral"
       WHERE "affiliate" = :affiliate
+        AND "Users"."id" = "Wallets"."userId"
       ORDER BY "timestamp" DESC
-    ) as events
-LIMIT :limit
-OFFSET :offset
+     ) as events
+LIMIT :limit OFFSET :offset;
