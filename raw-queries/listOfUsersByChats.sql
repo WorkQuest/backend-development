24baac30cb509d4b1683c135088d00aae48a32dd
Mixrosxema <RobertGtr@protonmail.com>
2022-03-18 14:40:54 +0700

    update queries

--- raw-queries/listOfUsersByChats.sql
+++ raw-queries/listOfUsersByChats.sql
@@ -1,39 +1,26 @@
-SELECT *
-FROM (SELECT
-             "event"."blockNumber",
-             "event"."transactionHash",
-             "event"."referral",
-             "event"."affiliate",
-             "event"."amount",
-             "event"."timestamp",
-             'PaidReferral'         as "event",
-             "referralUser"."firstName" as "referralUser.firstName",
-             "referralUser"."lastName" as "referralUser.lastName",
-             "referralUser->avatar"."url"         as "referralUser.avatar.url",
-             "referralUser->avatar"."contentType" as "referralUser.avatar.contentType"
-      FROM "ReferralProgramEventPaidReferrals" as "event"
-               LEFT JOIN "Wallets" as "referralWallet" ON "referralWallet"."address" = "referral"
-               LEFT JOIN "Users" as "referralUser" ON "referralUser"."id" = "referralWallet"."userId"
-               LEFT JOIN "Media" as "referralUser->avatar" ON "referralUser->avatar"."id" = "referralUser"."avatarId"
-      WHERE "event"."affiliate" = :affiliate
-      UNION
-      SELECT
-             "event"."blockNumber",
-             "event"."transactionHash",
-             null                   as "referral",
-             "event"."affiliate",
-             "event"."amount",
-             "event"."timestamp",
-             'RewardClaimed'        as "event",
-             "affiliateUser"."firstName" as "affiliateUser.firstName",
-             "affiliateUser"."lastName" as "affiliateUser.lastName",
-             "affiliateUser->avatar"."url"         as "affiliateUser.avatar.url",
-             "affiliateUser->avatar"."contentType" as "affiliateUser.avatar.contentType"
-      FROM "ReferralProgramEventRewardClaimeds" as "event"
-               LEFT JOIN "Wallets" as "affiliateWallet" ON "affiliateWallet"."address" = "affiliate"
-               LEFT JOIN "Users" as "affiliateUser" ON "affiliateUser"."id" = "affiliateWallet"."userId"
-               LEFT JOIN "Media" as "affiliateUser->avatar" ON "affiliateUser->avatar"."id" = "affiliateUser"."avatarId"
-      WHERE "event"."affiliate" = :affiliate
-      ORDER BY "timestamp" DESC
-     ) as events
-LIMIT :limit OFFSET :offset;
+SELECT
+    DISTINCT ON ("User"."id") "User"."id",
+    "User"."firstName",
+    "User"."lastName",
+    "User"."additionalInfo",
+    "User"."avatarId",
+    "avatar"."id"                                        AS "avatar.id",
+    "avatar"."url"                                       AS "avatar.url",
+    "avatar"."contentType"                               AS "avatar.contentType"
+FROM "Users" AS "User"
+    LEFT OUTER JOIN "Media" AS "avatar" ON "User"."avatarId" = "avatar"."id"
+
+    INNER JOIN "ChatMembers" AS "chatMember" ON "User"."id" = "chatMember"."userId"
+
+    INNER JOIN "Chats" AS "chatMember->chat" ON "chatMember"."chatId" = "chatMember->chat"."id"
+    AND "chatMember->chat"."type" IN ('private', 'quest')
+WHERE
+    ("User"."deletedAt" IS NULL)
+  AND "User".id != :currentUserId
+  AND "chatMember->chat"."id" IN (
+    SELECT "chatId" FROM "ChatMembers" WHERE "ChatMembers"."userId"=:currentUserId
+    )
+  AND "User"."id" NOT IN (
+    SELECT "ChatMembers"."userId" FROM "ChatMembers" WHERE "ChatMembers"."chatId"=:excludeUsersFromChatId
+    )
+    LIMIT :limitValue OFFSET :offsetValue;
--- raw-queries/referralProgramClaimAndPaidEvents.sql
+++ raw-queries/referralProgramClaimAndPaidEvents.sql
@@ -1,41 +1,37 @@
 SELECT *
-FROM (SELECT "blockNumber",
-             "transactionHash",
-             "referral",
-             "affiliate",
-             "amount",
-             "timestamp",
-             'PaidReferral'         as "event",
-             "firstName",
-             "lastName",
-             "avatar"."id"          as "avatar.id",
-             "avatar"."url"         as "avatar.url",
-             "avatar"."contentType" as "avatar.contentType"
-      FROM "Users"
-               LEFT JOIN "Media" as "avatar" ON "avatar"."id" = "Users"."avatarId",
-           "ReferralProgramEventPaidReferrals"
-               INNER JOIN "Wallets" ON "Wallets"."address" = "referral"
-      WHERE "affiliate" = :affiliate
-        AND "Users"."id" = "Wallets"."userId"
+FROM (SELECT "event"."blockNumber"                AS "blockNumber",
+             "event"."transactionHash"            AS "transactionHash",
+             "event"."referral"                   AS "referral",
+             "event"."affiliate"                  AS "affiliate",
+             "event"."amount"                     AS "amount",
+             "event"."timestamp"                  AS "timestamp",
+             'PaidReferral'                       AS "event",
+             "referralUser"."firstName"           AS "referralUser.firstName",
+             "referralUser"."lastName"            AS "referralUser.lastName",
+             "referralUser->avatar"."url"         AS "referralUser.avatar.url",
+             "referralUser->avatar"."contentType" AS "referralUser.avatar.contentType"
+      FROM "ReferralProgramEventPaidReferrals" AS "event"
+               LEFT JOIN "Wallets" AS "referralWallet" ON "referralWallet"."address" = "referral"
+               LEFT JOIN "Users" AS "referralUser" ON "referralUser"."id" = "referralWallet"."userId"
+               LEFT JOIN "Media" AS "referralUser->avatar" ON "referralUser->avatar"."id" = "referralUser"."avatarId"
+      WHERE "event"."affiliate" = :affiliate
       UNION
-      SELECT "blockNumber",
-             "transactionHash",
-             null                   as "referral",
-             "affiliate",
-             "amount",
-             "timestamp",
-             'RewardClaimed'        as "event",
-             "firstName",
-             "lastName",
-             "avatar"."id"          as "avatar.id",
-             "avatar"."url"         as "avatar.url",
-             "avatar"."contentType" as "avatar.contentType"
-      FROM "Users"
-               LEFT JOIN "Media" as "avatar" ON "avatar"."id" = "Users"."avatarId",
-           "ReferralProgramEventPaidReferrals"
-               INNER JOIN "Wallets" ON "Wallets"."address" = "referral"
-      WHERE "affiliate" = :affiliate
-        AND "Users"."id" = "Wallets"."userId"
+      SELECT "event"."blockNumber"                 AS "blockNumber",
+             "event"."transactionHash"             AS "transactionHash",
+             null                                  AS "referral",
+             "event"."affiliate"                   AS "affiliate",
+             "event"."amount"                      AS "amount",
+             "event"."timestamp"                   AS "timestamp",
+             'RewardClaimed'                       AS "event",
+             "affiliateUser"."firstName"           AS "affiliateUser.firstName",
+             "affiliateUser"."lastName"            AS "affiliateUser.lastName",
+             "affiliateUser->avatar"."url"         AS "affiliateUser.avatar.url",
+             "affiliateUser->avatar"."contentType" AS "affiliateUser.avatar.contentType"
+      FROM "ReferralProgramEventRewardClaimeds" as "event"
+               LEFT JOIN "Wallets" AS "affiliateWallet" ON "affiliateWallet"."address" = "affiliate"
+               LEFT JOIN "Users" AS "affiliateUser" ON "affiliateUser"."id" = "affiliateWallet"."userId"
+               LEFT JOIN "Media" AS "affiliateUser->avatar" ON "affiliateUser->avatar"."id" = "affiliateUser"."avatarId"
+      WHERE "event"."affiliate" = :affiliate
       ORDER BY "timestamp" DESC
      ) as events
-LIMIT :limit OFFSET :offset;
+    LIMIT :limit OFFSET :offset;
