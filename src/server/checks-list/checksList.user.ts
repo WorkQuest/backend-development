6e9d0fada93345967086133cefcbe9618a8ff486
Irina Prilepova <prilepovaid@mail.ru>
2022-04-11 11:29:13 +0700

    refactor(questResponse): add checker to check user rating statistic

--- src/server/checks-list/checksList.user.ts
+++ src/server/checks-list/checksList.user.ts
@@ -1,9 +1,10 @@
 import { error } from '../utils';
 import { Errors } from '../utils/errors';
 import {
+  ProfileVisibilitySetting, RatingStatus,
   User,
-  UserRole,
-} from '@workquest/database-models/lib/models';
+  UserRole
+} from "@workquest/database-models/lib/models";
 
 export class ChecksListUser {
   constructor(
@@ -16,6 +17,23 @@ export class ChecksListUser {
       throw error(Errors.InvalidStatus, "User role doesn't match", {
         current: this.user.role,
         mustHave: role,
+      });
+    }
+
+    return this;
+  }
+
+  public async ratingShouldCoincide(comparableUser: User): Promise<this> {
+    const profileVisibility = await ProfileVisibilitySetting.findOne({ where: { userId: this.user.id } });
+
+    if (profileVisibility.ratingStatus === RatingStatus.AllStatuses) {
+      return this;
+    }
+    if (this.user.ratingStatistic.status !== profileVisibility.ratingStatus) {
+      throw error(Errors.InvalidStatus, `User rating doesn't coincide to profile visibility setting of ${comparableUser.id}`, {
+        userId: this.user.id,
+        currentRatingStatus: this.user.ratingStatistic.status,
+        coincideUserRatingStatus: profileVisibility.ratingStatus,
       });
     }
 
