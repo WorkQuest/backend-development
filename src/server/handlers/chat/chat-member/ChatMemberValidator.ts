2e11ca9176459590ba54e878c3bcefcb5e1b93f1
Irina Prilepova <prilepovaid@mail.ru>
2022-05-16 13:04:01 +0700

    refactor(chat): add chatMember checker

--- src/server/handlers/chat/chat-member/ChatMemberValidator.ts
+++ src/server/handlers/chat/chat-member/ChatMemberValidator.ts
@@ -1,7 +1,14 @@
 import { Chat, ChatMember } from '@workquest/database-models/lib/models';
+import { error } from "../../../utils";
+import { Errors } from "../../../utils/errors";
 
 export class ChatMemberValidator {
   public NotNull(chat: Chat, member: ChatMember) {
-
+    if (!chat.members.includes(member)) {
+      throw error(Errors.Forbidden, 'User is not a member of this chat', {
+        chatId: chat.id,
+        userId: member.userId,
+      });
+    }
   }
 }
