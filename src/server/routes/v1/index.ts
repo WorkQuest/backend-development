9fc2e99e4d3b29d06f40e4e12889f25ca37e0492
Ilya Kasimov <adamoffilya@mail.ru>
2022-05-30 16:04:38 +0700

    Add report create function

--- src/server/routes/v1/index.ts
+++ src/server/routes/v1/index.ts
@@ -19,6 +19,7 @@ import proposal from './proposal';
 import pensionFund from './pensionFund';
 import referral from './referral';
 import savingProduct from "./savingProduct";
+import report from './report';
 
 export default [
   ...liquidityPoolWETH,
@@ -41,5 +42,6 @@ export default [
   ...proposal,
   ...pensionFund,
   ...referral,
-  ...savingProduct
+  ...savingProduct,
+  ...report
 ];
