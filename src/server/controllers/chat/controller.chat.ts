a59134c313c2345a10d6c9cae88174aba7e9d36f
Mixrosxema <RobertGtr@protonmail.com>
2022-05-31 16:25:46 +0700

    update project

--- src/server/controllers/chat/controller.chat.ts
+++ src/server/controllers/chat/controller.chat.ts
@@ -143,38 +143,6 @@ export class ChatController {
   }
 }
 
-abstract class MessageHelper {
-  public abstract message: Message;
-
-  async messageMustBeSender(userId: string) {
-    if (this.message.senderMemberId !== userId) {
-      throw error(Errors.Forbidden, "User isn't sender of the message", {
-        messageId: this.message.id,
-      });
-    }
-
-    return this;
-  }
-
-  async messageMustBeChat(chatId: string) {
-    if (this.message.chatId !== chatId) {
-      throw error(Errors.Forbidden, 'This message not from this group-chat', {});
-    }
-
-    return this;
-  }
-}
-
-export class MessageController extends MessageHelper {
-  constructor(public message: Message) {
-    super();
-
-    if (!message) {
-      throw error(Errors.NotFound, 'Message not found', {});
-    }
-  }
-}
-
 export class QuestChatController extends ChatController {
   constructor(
     public readonly chat: Chat,
